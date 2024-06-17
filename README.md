a552b81d8edff8cd15fe90c55968522d404c9929
krozfu <carlosuefagh@gmail.com>
2024-06-16 23:28:26 -0500

    changes optimizate code main.py

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
+# Creadores
+```python
+# Información
+> Juan Carlos Charfuelan
+> Juan David Arias
+```
+
+![img](./src/assets/img/img_2.png)
+
 # Pedagogical-Phishing
-Este proyecto está diseñado para educar sobre los correos de phishing, proporcionando ejemplos y prácticas para identificar y evitar estos correos maliciosos.
+Este proyecto está diseñado para educar sobre los correos de phishing, proporcionando ejemplos y prácticas para identificar y evitar estos correos maliciosos, esta aplicación esta realizada con el lenguaje de programación Python, utilizando una librería de yagmail, para el envió de correos.
 
 ## Tabla de Contenidos
 * [Descripción](#Descripción)
 * [Instalación](#Instalación)
-* [Instalación](#Ejecución)
+    * [Crear un ambiente virtual](#Crear)
+    * [Activar el ambiente virtual](#Activar)
+* [Configuración](#Configuración)
+* [Ejecución](#Ejecución)
 
 ## Descripción
 Pedagogical-Phishing es una herramienta educativa diseñada para enseñar a los usuarios a identificar y evitar correos electrónicos de phishing. El proyecto incluye ejemplos prácticos y ejercicios interactivos que ayudan a los usuarios a reconocer los signos comunes de los correos de phishing. Estos ejercicios pueden enviarse a una lista de correos disponibles, como los de una misma empresa, con el objetivo de auditar y evaluar la respuesta ante intentos de phishing.
@@ -33,6 +45,13 @@ Instala los paquetes necesarios especificados en `requirements.txt`:
 ```bash
 pip install -r requirements.txt
 ```
+## Configuración
+Antes de ejecutar el `script.sh` se debe de configurar el archivo `.env`, con las variables de entorno para se administradas con Python, estas credenciales son generadas con anterioridad desde el correo remitente, par poder realizar este proceso.
+```bash
+EMAIL_REMITENTE='gmail-remitente'
+EMAIL_CONTRASENA='credencial-aplicacion-para-enviar-correos'
+```
+También se debe de tener una lista de emails a los cuales se van a enviar los correos de phishing, la lista debe de llamarse `emails.txt`, cada emails debe de ser ingresado con saldo de línea.
 
 ## Ejecución
 Para ejecutar el codigo debes de dar permiso de ejecucion al los archivos de `script.sh` y `main.py`.
@@ -41,3 +60,13 @@ Para ejecutar el codigo debes de dar permiso de ejecucion al los archivos de `sc
 chmod +x script.sh
 chmod +x main.py
 ```
+
+Ejecución de script de Bash
+```bash
+./script.h
+```
+
+## Ejemplo
+El aplicativo envía un correo con el siguiente cuerpo dependiendo de la organización y destinatario a enviar.
+
+![img](./src/assets/img/img_1.png)
--- main.py
+++ main.py
@@ -1,77 +1,87 @@
-# Utilizacion de la libreari yagmail para el envio de correos
 import random
 import yagmail
+import os
+from dotenv import load_dotenv
+
+# Definimos constantes
+TIPOS_PHISHING = {
+    "1": "Amazon",
+    "2": "Facebook",
+    "3": "Outlook",
+    "4": "Gmail"
+}
+    
+SALUDOS_Y_CUERPOS = {
+    "Amazon": {
+        "saludos": ["Estimado Usuario Amazon", "Estimado Cliente", "Hola"],
+        "cuerpo": """
+            Estimado usuario Amazon,
+
+            Hemos detectado actividad inusual en su cuenta. Para garantizar la seguridad de su información, por favor haga clic en el siguiente enlace y cambie su contraseña de inmediato:
+
+            [Enlace fraudulento]
+
+            Gracias por su cooperación.
+
+            Atentamente,
+            El equipo de soporte técnico
+        """
+    },
+    "Facebook": {
+        "saludos": ["Estimado usuario Facebook", "Hola"],
+        "cuerpo": """
+            Estimado usuario Facebook,
+
+            Hemos detectado actividad inusual en su cuenta. Para garantizar la seguridad de su información, por favor haga clic en el siguiente enlace y cambie su contraseña de inmediato:
+
+            [Enlace fraudulento]
+
+            Gracias por su cooperación.
+
+            Atentamente,
+            El equipo de soporte técnico
+        """
+    },
+    "Outlook": {
+        "saludos": ["Estimado usuario Outlook", "Hola"],
+        "cuerpo": """
+            Estimado usuario Outlook,
+
+            Hemos detectado actividad inusual en su cuenta. Para garantizar la seguridad de su información, por favor haga clic en el siguiente enlace y cambie su contraseña de inmediato:
+
+            [Enlace fraudulento]
+
+            Gracias por su cooperación.
+
+            Atentamente,
+            El equipo de soporte técnico
+        """
+    },
+    "Gmail": {
+        "saludos": ["Estimado usuario Gmail", "Hola"],
+        "cuerpo": """
+            Estimado usuario Gmail,
+
+            Hemos detectado actividad inusual en su cuenta. Para garantizar la seguridad de su información, por favor haga clic en el siguiente enlace y cambie su contraseña de inmediato:
+
+            [Enlace fraudulento]
+
+            Gracias por su cooperación.
+
+            Atentamente,
+            El equipo de soporte técnico
+        """
+    }
+}
+
+ASUNTO = "Urgente: Información de cuenta comprometida"
 
 def generar_correo(phishing_type):
-    saludos_y_cuerpos = {
-        "Amazon": {
-            "saludos": ["Estimado Usuario Amazon", "Estimado Cliente", "Hola"],
-            "cuerpo": """
-                Estimado usuario Amazon,
-
-                Hemos detectado actividad inusual en su cuenta. Para garantizar la seguridad de su información, por favor haga clic en el siguiente enlace y cambie su contraseña de inmediato:
-
-                [Enlace fraudulento]
-
-                Gracias por su cooperación.
-
-                Atentamente,
-                El equipo de soporte técnico
-                """
-        },
-        "Facebook": {
-            "saludos": ["Estimado usuario Facebook", "Hola"],
-            "cuerpo": """
-                Estimado usuario Facebook,
-
-                Hemos detectado actividad inusual en su cuenta. Para garantizar la seguridad de su información, por favor haga clic en el siguiente enlace y cambie su contraseña de inmediato:
-
-                [Enlace fraudulento]
-
-                Gracias por su cooperación.
-
-                Atentamente,
-                El equipo de soporte técnico
-                """
-        },
-        "Outlook": {
-            "saludos": ["Estimado usuario Outlook", "Hola"],
-            "cuerpo": """
-                Estimado usuario Outlook,
-
-                Hemos detectado actividad inusual en su cuenta. Para garantizar la seguridad de su información, por favor haga clic en el siguiente enlace y cambie su contraseña de inmediato:
-
-                [Enlace fraudulento]
-
-                Gracias por su cooperación.
-
-                Atentamente,
-                El equipo de soporte técnico
-                """
-        },
-        "Gmail": {
-            "saludos": ["Estimado usuario Gmail", "Hola"],
-            "cuerpo": """
-                Estimado usuario Gmail,
-
-                Hemos detectado actividad inusual en su cuenta. Para garantizar la seguridad de su información, por favor haga clic en el siguiente enlace y cambie su contraseña de inmediato:
-
-                [Enlace fraudulento]
-
-                Gracias por su cooperación.
-
-                Atentamente,
-                El equipo de soporte técnico
-                """
-        }
-    }
-
-    if phishing_type in saludos_y_cuerpos:
-        datos = saludos_y_cuerpos[phishing_type]
+    if phishing_type in SALUDOS_Y_CUERPOS:
+        datos = SALUDOS_Y_CUERPOS[phishing_type]
         saludo = random.choice(datos["saludos"])
-        asunto = "Urgente: Información de cuenta comprometida"
         cuerpo = datos["cuerpo"].strip()
-        return saludo, asunto, cuerpo
+        return saludo, ASUNTO, cuerpo
     else:
         raise ValueError("Tipo de phishing no válido")
 
@@ -84,36 +94,49 @@ def enviar_correo(destinatarios, asunto, cuerpo, remitente, contrasena):
     except Exception as e:
         print(f"Error al enviar correo: {str(e)}")
 
-def main():
+def seleccionar_tipo_phishing():
     print("Seleccione el tipo de phishing a generar:")
-    print("1. Amazon")
-    print("2. Facebook")
-    print("3. Outlook")
-    print("4. Gmail")
+    for key, value in TIPOS_PHISHING.items():
+        print(f"{key}. {value}")
     
     opcion = input("Ingrese el número de la opción deseada: ").strip()
-    
-    opciones = {
-        "1": "Amazon",
-        "2": "Facebook",
-        "3": "Outlook",
-        "4": "Gmail"
-    }
-    
-    tipo_phishing = opciones.get(opcion)
+    return TIPOS_PHISHING.get(opcion)
+
+def cargar_destinatarios(nombre_archivo):
+    try:
+        with open(nombre_archivo, 'r') as archivo:
+            destinatarios = [linea.strip() for linea in archivo if linea.strip()]
+        return destinatarios
+    except FileNotFoundError:
+        print(f"Archivo {nombre_archivo} no encontrado.")
+        return []
+    except Exception as e:
+        print(f"Error al leer el archivo {nombre_archivo}: {str(e)}")
+        return []
+
+def main():
+    tipo_phishing = seleccionar_tipo_phishing()
     
     if tipo_phishing:
         try:
             saludo, asunto, cuerpo = generar_correo(tipo_phishing)
             print("Enviando...")
-            
-            remitente = 'xxxxxxxxxxxxx@xxx.xx'  # Cambiar al correo proporcionado
-            contrasena = 'xxxxxxxxxxxxx'  # Aquí deberías usar una contraseña real y segura
-            
-            # Lista de destinatarios
-            destinatarios = ['xxxxxxxxxxxxx@xxx.xx']
 
-            enviar_correo(destinatarios, asunto, cuerpo, remitente, contrasena)
+            load_dotenv()
+            remitente = os.getenv('EMAIL_REMITENTE')
+            contrasena = os.getenv('EMAIL_CONTRASENA')
+            
+            if not remitente or not contrasena:
+                print("Remitente o contraseña no configurados. Asegúrese de configurar las variables de entorno 'EMAIL_REMITENTE' y 'EMAIL_CONTRASENA'.")
+                return
+
+            nombre_archivo = 'emails.txt'
+            destinatarios = cargar_destinatarios(nombre_archivo)
+            
+            if destinatarios:
+                enviar_correo(destinatarios, asunto, cuerpo, remitente, contrasena)
+            else:
+                print("No se encontraron destinatarios válidos.")
         
         except ValueError as e:
             print(e)
